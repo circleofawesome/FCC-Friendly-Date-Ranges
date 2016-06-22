a015d0b8438155cd6b91117649815fc574c5bee9
Samiul Anwar <samiul.anwar@gmail.com>
2016-06-22 12:26:41 -0400

    date converting function fully functional

--- methods.js
+++ methods.js
@@ -4,6 +4,19 @@ function numToDate(num,currentYear){
 	var months={'01':'January','02':'February','03':'March','04':'April','05':'May','06':'June','07':'July','08':'August','09':'September','10':'October','11':'November','12':'December'};
 	if(currentYear){
 		//return only the month and day if currentYear is true
+		num=num.split('-');
+		if(num[2]==='01'){
+			return months[num[1]]+" "+parseInt(num[2])+"st";
+		}
+		else if(num[2]==='02'){
+			return months[num[1]]+" "+parseInt(num[2])+"nd";
+		}
+		else if(num[2]==='03'){
+			return months[num[1]]+" "+parseInt(num[2])+"rd";
+		}
+		else{
+			return months[num[1]]+" "+parseInt(num[2])+"th";
+		}
 	}
 	else{
 		//return the whole date
