87659b66530b44995704cc0b220cec15423b638a
Samiul Anwar <samiul.anwar@gmail.com>
2016-06-22 12:00:09 -0400

    initial commit

---
+++ methods.js
@@ -0,0 +1,11 @@
+//takes a string date and converts it to a friendly date
+
+function numToDate(num,currentYear){
+	var months={'01':'January','02':'February','03':'March'};
+	if(currentYear){
+		//return only the month and day
+	}
+	else{
+		//return the whole date
+	}
+}
